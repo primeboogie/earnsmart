f1844ecb93d61a82f9898016f6ba17ece35c6a10
primeboogie <amososwom@gmail.com>
2024-07-22 03:09:14 +0300

    Updated

--- utils/auth.js
+++ utils/auth.js
@@ -50,6 +50,5 @@ openLoader(false)
 menuid.addEventListener('click', () => {
     menuid.classList.toggle("fa-xmark")
     navbar.classList.toggle("navhelper")
-    overs.classList.toggle("showall")
 })
 
