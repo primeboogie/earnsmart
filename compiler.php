91bb0711010281f10a868803f68f52e7cca94e8b
primeboogie <amososwom@gmail.com>
2024-08-02 22:25:03 +0300

    Updated Dashboard

--- compiler.php
+++ compiler.php
@@ -11,6 +11,19 @@
   <div class="dot"></div>
   <div class="dot"></div>
 </section>
+<div class="mylink">
+    <span  class="usernames">Hi <i id="username"></i></span>
+    <div class="linkdiv">
+        <p>
+             Welcome to Earn-Empire Connections Your Affilate Link is Active,
+            Please Copy And Share to Your Downlines
+        </p>
+        <div class="linkinputdiv">
+        <input type="text" id="linkinput" readonly value="<?php echo $admin['domain'].'/reg/'?>" disbaled>
+        <button id="linkbtn"><i class="fa-solid fa-link"></i> Copy Link</button>
+        </div>
+    </div>
+</div>
 
     <div class="root">
         <div class="header">
