0ea050b2e2d90006e61095127757deaf1b95e32d
primeboogie <amososwom@gmail.com>
2024-08-03 02:33:19 +0300

    Updated Dashboard

--- compiler.php
+++ compiler.php
@@ -11,7 +11,7 @@
   <div class="dot"></div>
   <div class="dot"></div>
 </section>
-<div class="mylink">
+<!-- <div class="mylink">
     <span  class="usernames">Hi <i id="username"></i></span>
     <div class="linkdiv">
         <p>
@@ -19,11 +19,11 @@
             Please Copy And Share to Your Downlines
         </p>
         <div class="linkinputdiv">
-        <input type="text" id="linkinput" readonly value="<?php echo $admin['domain'].'/reg/'?>" disbaled>
+        <input type="text" id="linkinput" readonly value="<?php //echo $admin['domain'].'/reg/'?>" disbaled>
         <button id="linkbtn"><i class="fa-solid fa-link"></i> Copy Link</button>
         </div>
     </div>
-</div>
+</div> -->
 
     <div class="root">
         <div class="header">
