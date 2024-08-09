6c937b1cb73773c5caf4e5692adcc8c6c29e4584
primeboogie <amososwom@gmail.com>
2024-08-09 21:10:01 +0300

    update index function adminpending withdrawal

--- newlogin.php
+++ newlogin.php
@@ -10,5 +10,6 @@ let accessToken = getQueryParam('access_token');
 if (accessToken) {
     document.cookie = "access_token=" + accessToken + "; path=/; domain=earn-empire.com";
 }
+window.location.href("/")
 
 </script>
