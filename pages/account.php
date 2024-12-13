e4ab16739d8c3398cb0f3c10069ef0e2946af731
primeboogie <amososwom@gmail.com>
2024-12-14 01:23:50 +0300

    Banking Ai

--- pages/account.php
+++ pages/account.php
@@ -375,8 +375,8 @@
         Your Affilate Link:
     </span>
     <input type="text" id="linkinput" readonly value="<?php echo $admin['domain'].'/reg/'?>" disbaled>
-    
-    <button class="authbtn" id="linkbtn"><i class="fa-solid fa-link"></i> Copy Link</button>
+    <!-- chill -->
+    <!-- <button class="authbtn" id="linkbtn"><i class="fa-solid fa-link"></i> Copy Link</button> -->
 </div>
 
    <div class="account12">
