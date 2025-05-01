0d964f5dc170d5d146e8d92b889f204980389801
primeboogie <amososwom@gmail.com>
2025-05-02 00:30:25 +0300

    updated eadme

--- utils/auth.js
+++ utils/auth.js
@@ -699,7 +699,7 @@ function grabpayment(data) {
               nb.innerHTML = `After payment, paste the transaction message along with your phone number, and email to our helpline: ${all[key][2]} <br>
               <br>
 
-              ⚠️ Warning: <br>
+              ℹ️ Polite Notice: <br>
                 * Don't send money to the helpline <br>
                 * Paste the message (Not Screenshot)
               `;
